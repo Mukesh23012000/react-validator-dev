403060b4a5fa216ec11244f16beb8f62f27be2a8
Mukesh <mukeshkannan.a@novactech.in>
2024-10-28 12:28:14 +0530

    version 1.2.0 and build files

--- dist/useValidation.d.ts
+++ dist/useValidation.d.ts
@@ -41,5 +41,5 @@ interface ValidationErrors {
 declare const useValidation: (data: {
     fields: Record<string, any>;
     validation: Validation;
-}, submitted?: boolean, debounceDelay?: number) => ValidationErrors[];
+}, isMultiple?: boolean, submitted?: boolean, debounceDelay?: number) => ValidationErrors[];
 export default useValidation;
